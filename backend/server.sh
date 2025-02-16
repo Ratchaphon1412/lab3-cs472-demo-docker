6eb7c7de1ed022c4f088ce84c2d198d558cbc788
Ratchaphon1412 <Ratchaphon.h111@gmail.com>
2025-02-17 00:45:35 +0700

    initial demo devops container

---
+++ backend/server.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+# create migrate database
+poetry run python manage.py migrate
+
+# clear all data in database
+poetry run python manage.py flush --no-input
+
+# mock playlist
+poetry run python manage.py playlist_mock
+
+# run server
+poetry run python manage.py runserver 0.0.0.0:8000
