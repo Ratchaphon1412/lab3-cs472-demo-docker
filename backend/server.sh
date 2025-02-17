74ebea0e74c9c79597b2b54042e855ac62d3c9da
Ratchaphon1412 <Ratchaphon.h111@gmail.com>
2025-02-17 16:24:15 +0700

    edit server.sh

--- backend/server.sh
+++ backend/server.sh
@@ -2,12 +2,9 @@
 
 # create migrate database
 poetry run python manage.py migrate
-
 # clear all data in database
 poetry run python manage.py flush --no-input
-
 # mock playlist
 poetry run python manage.py playlist_mock
-
 # run server
 poetry run python manage.py runserver 0.0.0.0:8000
