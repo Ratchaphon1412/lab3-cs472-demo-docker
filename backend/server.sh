c4806ebda46008cbee76678efb77dbab9ad218f5
Ratchaphon1412 <Ratchaphon.h111@gmail.com>
2025-02-17 16:40:40 +0700

    fix static

--- backend/server.sh
+++ backend/server.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# create migrate database
-poetry run python manage.py migrate
-# clear all data in database
-poetry run python manage.py flush --no-input
-# mock playlist
-poetry run python manage.py playlist_mock
-# run server
-poetry run python manage.py runserver 0.0.0.0:8000
+poetry run python manage.py migrate && poetry run python manage.py flush --no-input && poetry run python manage.py playlist_mock && poetry run python manage.py runserver 0.0.0.0:8000 --nostatic
