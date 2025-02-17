476ba48d4af456ace46cd591baa0b538371260ea
Ratchaphon1412 <Ratchaphon.h111@gmail.com>
2025-02-17 19:00:36 +0700

    fix bug

--- frontend/data/albums.ts
+++ frontend/data/albums.ts
@@ -6,47 +6,54 @@ export interface Album {
 }
 
 export interface makeForYouAlbum {
-  name: string
-  artist: string
-  cover: string
+  id: number
+  description: string
+  image : string
+  name : string
 }
 
   
   export const madeForYouAlbums: makeForYouAlbum[] = [
     {
+      id: 1,
       name: 'Thinking Components',
-      artist: 'Lena Logic',
-      cover:
+      description: 'Lena Logic',
+      image:
         'https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80',
     },
     {
+      id: 2,
       name: 'Functional Fury',
-      artist: 'Beth Binary',
-      cover:
+      description: 'Beth Binary',
+      image:
         'https://images.unsplash.com/photo-1513745405825-efaf9a49315f?w=300&dpr=2&q=80',
     },
     {
+      id: 3,
       name: 'React Rendezvous',
-      artist: 'Ethan Byte',
-      cover:
+      description: 'Ethan Byte',
+      image:
         'https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80',
     },
     {
+      id: 4,
       name: 'Stateful Symphony',
-      artist: 'Beth Binary',
-      cover:
+      description: 'Beth Binary',
+      image:
         'https://images.unsplash.com/photo-1446185250204-f94591f7d702?w=300&dpr=2&q=80',
     },
     {
+      id: 5,
       name: 'Async Awakenings',
-      artist: 'Nina Netcode',
-      cover:
+      description: 'Nina Netcode',
+      image:
         'https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80',
     },
     {
+      id: 6,
       name: 'The Art of Reusability',
-      artist: 'Lena Logic',
-      cover:
+      description: 'Lena Logic',
+      image:
         'https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80',
     },
   ]
